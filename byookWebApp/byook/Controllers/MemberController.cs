2e8e4da73e628b05989d583567fc784b031342a1
cjk <vmflsxj0@naver.com>
2022-06-12 16:55:27 +0900

    소비자 및 판매자 회원가입 화면 수정

---
+++ byookWebApp/byook/Controllers/MemberController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace byook.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult ConsumerJoin()
+        {
+            return View();
+        }
+
+        public IActionResult SellerJoin()
+        {
+            return View();
+        }
+
+        public IActionResult SelectJoin()
+        {
+            return View();
+        }
+    }
+}
